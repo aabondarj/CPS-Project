d6a7be7b4b46217777288c5026517274fb9224e7
“Aleksei <wx200223@gmail.com>
2023-08-04 15:29:12 +0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import '../scss/style.scss';
+import callForm from './path/to/callform';
+import feedbackForm from './path/to/feedbackform';
+import leftMenu from './path/to/leftMenu';
+import mainBrands from './path/to/main__brands';
+import swiper from './path/to/swiper';
+
+console.log('Works!')
